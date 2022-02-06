ab81e5714ccb877ba2ff794179127b3b37a8edc4
minsnew <minsnew@naver.com>
2022-02-06 11:03:09 +0900

    Add redux toolkit (#7)
    * feat: add redux & redux toolkit
    
    * feat: add redux toolkit sample using tutorial
    
    * feat: add counter reducer in main page

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,7 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {
+  decrement,
+  increment,
+  incrementAsync,
+  incrementByAmount,
+  incrementIfOdd,
+  selectCount,
+} from '../../store/CounterSlice/CounterSlice';
+import { useAppDispatch, useAppSelector } from '../../store/hooks';
+import styles from './MainPage.module.css';
 
 const MainPage: React.FC = () => {
-  return <div>Main Page</div>;
+  const count = useAppSelector(selectCount);
+  const dispatch = useAppDispatch();
+  const [incrementAmount, setIncrementAmount] = useState('2');
+
+  const incrementValue = Number(incrementAmount) || 0;
+
+  return (
+    <div>
+      <div className={styles.row}>
+        <button className={styles.button} aria-label="Decrement value" onClick={() => dispatch(decrement())}>
+          -
+        </button>
+        <span className={styles.value}>{count}</span>
+        <button className={styles.button} aria-label="Increment value" onClick={() => dispatch(increment())}>
+          +
+        </button>
+      </div>
+      <div className={styles.row}>
+        <input
+          className={styles.textbox}
+          aria-label="Set increment amount"
+          value={incrementAmount}
+          onChange={(e) => setIncrementAmount(e.target.value)}
+        />
+        <button className={styles.button} onClick={() => dispatch(incrementByAmount(incrementValue))}>
+          Add Amount
+        </button>
+        <button className={styles.asyncButton} onClick={() => dispatch(incrementAsync(incrementValue))}>
+          Add Async
+        </button>
+        <button className={styles.button} onClick={() => dispatch(incrementIfOdd(incrementValue))}>
+          Add If Odd
+        </button>
+      </div>
+    </div>
+  );
 };
 MainPage.displayName = 'MainPage';
 
